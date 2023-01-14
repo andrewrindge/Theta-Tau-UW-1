15d8eea64dd658d862a0a2238ac7385995744d59
patrickliu022 <patrickliu022@gmail.com>
2023-01-14 03:12:58 -0800

    finished large information banner

--- theta-tau-uw/components/ui/LargeInformationBanner.tsx
+++ theta-tau-uw/components/ui/LargeInformationBanner.tsx
@@ -16,58 +16,60 @@ export default function LargeInformationBanner({ data }: Props) {
             width='100%'
             padding='25px'
             paddingTop='100px'
+            backgroundColor='colors.200'
+            alignItems='center'
         >
-            <MarginStack>
+            <Stack
+                direction={{ base: 'column', md: 'row' }}
+                width={{ base: '100%', md: '85%' }}
+            >
                 <Stack
-                    direction={{ base: 'column', sm: 'row' }}
-                    width={{ base: '100%', md: '85%' }}
+                    width='100%'
+                    height='inherit'
+                    justifyContent={data.button ? 'space-between' : 'center'}
+                    padding='15px'
+                    flex={1}
+                    gap='25px'
                 >
-                    <Stack
-                        width='100%'
-                        height='inherit'
-                        justifyContent={data.button ? 'space-between' : 'center'}
-                        padding='15px'
-                        flex={1}
-                        border='1px solid #000'
+                    <Text fontSize='36px' fontWeight={700}>
+                        {data.title}
+                    </Text>
+                    <Text
+                        fontWeight={500}
+                        fontSize='16px'
+                        textAlign='justify'
+                        width='90%'
                     >
-                        <Text fontSize='36px' fontWeight={700}>
-                            {data.title}
-                        </Text>
-                        <Text
-                            fontWeight={500}
-                            fontSize='16px'
-                            textAlign='justify'
+                        {data.description}
+                    </Text>
+                    {data.button && (
+                        <Button
+                            fontSize={{ base: '15px', md: '18px' }}
+                            padding='20px'
+                            backgroundColor='#FFF'
+                            border='1px solid #000'
+                            borderRadius='10px'
+                            width='fit-content'
+                            onClick={() => router.push(data.button?.link as string)}
                         >
-                            {data.description}
-                        </Text>
-                        {data.button && (
-                            <Button
-                                fontSize='18px'
-                                padding='20px'
-                                backgroundColor='#FFF'
-                                border='1px solid #000'
-                                borderRadius='10px'
-                                width='30%'
-                                onClick={() => router.push(data.button?.link as string)}
-                            >
-                                {data.button.title}
-                            </Button>
-                        )}
-                    </Stack>
-                    <Stack
-                        flex={1}
-                        justifyContent='center'
-                    >
-                        <Image
-                            src={data.image.src}
-                            alt={data.image.alt}
-                            borderRadius='5px'
-                            boxShadow='1px 1px 5px #888'
-                            objectFit='cover'
-                        />
-                    </Stack>
+                            {data.button.title}
+                        </Button>
+                    )}
                 </Stack>
-            </MarginStack>
+                <Stack
+                    flex={1}
+                    justifyContent='center'
+                >
+                    <Image
+                        src={data.image.src}
+                        alt={data.image.alt}
+                        borderRadius='5px'
+                        boxShadow='1px 1px 5px #888'
+                        objectFit='cover'
+                    />
+                </Stack>
+            </Stack>
+
         </Stack>
     )
 }
